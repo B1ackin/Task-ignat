8aa1a0850907b169dbaeec50655fe7172d22e887
B1ackin <xx13.02@gmail.com>
2020-07-10 18:27:10 +0300

    Updates

---
+++ precache-manifest.15ded2efe242559ab8ca24a4edaafcf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e59067527a4b5e840cf4f6289e69e5f",
+    "url": "/Task-ignat/index.html"
+  },
+  {
+    "revision": "b8c82a0bb6df6e045986",
+    "url": "/Task-ignat/static/css/main.a8a42c10.chunk.css"
+  },
+  {
+    "revision": "84255d5ba4d21025eaf7",
+    "url": "/Task-ignat/static/js/2.58114d01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-ignat/static/js/2.58114d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c82a0bb6df6e045986",
+    "url": "/Task-ignat/static/js/main.cc972241.chunk.js"
+  },
+  {
+    "revision": "0c34b6b31f19f696851e",
+    "url": "/Task-ignat/static/js/runtime-main.e3324afd.js"
+  },
+  {
+    "revision": "b09f37e37bc73385ad5130e99c961811",
+    "url": "/Task-ignat/static/media/minion.b09f37e3.jpg"
+  }
+]);
